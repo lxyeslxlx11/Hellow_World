0bd02c63464be7d278ca618e5a02f45f06e807a0
Talon <lxyeslxlx13@163.com>
2013-12-19 19:39:22 +0800

    add order.html processorder

---
+++ processorder.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Bob's Auto Patrs - Order Results</title>
+	</head>
+	<body>
+		<h1>Bob 's Auto Parts</h1>
+		<h2>Order Results</h2>
+		<p>Order processed.</p>
+	</body>
+</html>
